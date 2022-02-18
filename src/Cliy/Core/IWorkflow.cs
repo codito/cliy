48525cbcb1f311b97dfeb2b8b012c6911ece9d3f
Arun Mahapatra <codito@codito.in>
2022-02-18 09:12:15 +0530

    feat: add directory props and targets. Enable stylecop.

---
+++ src/Cliy/Core/IWorkflow.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Arun Mahapatra. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cliy.Core;
+
+internal interface IWorkflow
+{
+}
