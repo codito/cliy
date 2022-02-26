459c290346bf4b11591ec74fa984f14f9c7a3af8
Arun Mahapatra <codito@codito.in>
2022-02-26 22:12:42 +0530

    feat: add template json, project and unit test.

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## Unreleased
+
+NA
+
+## v1.0.0 - 2021-04-17
+
+- Fix: add a link to the bug fixed.
+- Feature: add a link to the feature docs.
+- First release of Cliy. Yay!
---
+++ template/test/UnitTest1.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Arun Mahapatra. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cliy.Template.Test;
+
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
