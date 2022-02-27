4f8f1ef8bdb449c7e518e3b560f5672c36fe4bcf
Arun Mahapatra <codito@codito.in>
2022-02-27 11:37:52 +0530

    fix: add uninstall step for test install of cliy template.

--- test/CliyTemplateTest.cs
+++ test/CliyTemplateTest.cs
@@ -5,22 +5,47 @@ namespace Cliy.Template.Tests;
 
 using Boxed.DotnetNewTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Threading.Tasks;
 
 [TestClass]
-public class CliyTemplateTest
+public class CliyTemplateTest : IDisposable
 {
     private const string TemplateSlnPath = "Cliy.sln";
+    private bool disposedValue;
+
+    public CliyTemplateTest() => DotnetNew.InstallAsync<CliyTemplateTest>(TemplateSlnPath).Wait();
 
     [TestMethod]
     public async Task DotnetRunWithDefaultArgumentsIsSuccessful()
     {
-        await DotnetNew.InstallAsync<CliyTemplateTest>(TemplateSlnPath);
         await using var tempDirectory = TempDirectory.NewTempDirectory();
         var project = await tempDirectory.DotnetNewAsync("cliy");
 
         await project.DotnetRestoreAsync();
         await project.DotnetBuildAsync();
         await project.DotnetTestAsync();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                DotnetNew.UninstallAsync<CliyTemplateTest>(TemplateSlnPath).Wait();
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
